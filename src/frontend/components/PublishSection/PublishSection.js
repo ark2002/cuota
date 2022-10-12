b9b829f91654597eca37281a4928c71146387e38
Aryak Lahane <67817330+ark2002@users.noreply.github.com>
2022-10-12 21:53:04 +0530

    feat : created PublishSection Component

---
+++ src/frontend/components/PublishSection/PublishSection.js
@@ -0,0 +1,15 @@
+import "./PublishSection.css";
+
+const PublishSection = () => {
+  return (
+    <div className="base-widget__top flex__row-center">
+      <span className="material-icons public__icon">public</span>
+      <div className="base-widget__top-details flex__column">
+        <p className="widget__text-large bold">Share to web</p>
+        <p className="widget__text-small">Publish and share link with anyone</p>
+      </div>
+    </div>
+  );
+};
+
+export { PublishSection };
