a1093103a05188b0edc11ba62f9a24164fd3adec
Aryak Lahane <67817330+ark2002@users.noreply.github.com>
2022-10-13 20:25:32 +0530

    refactor : updated BaseWidget according to context

--- src/frontend/components/BaseWidget/BaseWidget.js
+++ src/frontend/components/BaseWidget/BaseWidget.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+import { useInvite } from "../../context/InviteContext";
 import { HelpSection } from "../HelpSection/HelpSection";
 import { InvitedUsersSection } from "../InvitedUsersSection/InvitedUsersSection";
 import { PublishSection } from "../PublishSection/PublishSection";
@@ -5,6 +7,15 @@ import { SearchInput } from "../SearchInput/SearchInput";
 import "./BaseWidget.css";
 
 const BaseWidget = ({ isWidget, setIsWidget }) => {
+  const [inviteList, setInviteList] = useState([]);
+  const {
+    list: { dataList },
+  } = useInvite();
+
+  useEffect(() => {
+    setInviteList(dataList.filter((user) => user.invited));
+  }, [dataList]);
+
   return (
     <div className="base-widget__container flex__column-center">
       <PublishSection />
@@ -12,10 +23,13 @@ const BaseWidget = ({ isWidget, setIsWidget }) => {
         onClick={() => setIsWidget("search")}
         className="base-widget-search__wrapper"
       >
-        <SearchInput isWidget={isWidget} />
+        <SearchInput isWidget={isWidget} inviteList={inviteList} />
       </div>
-      <InvitedUsersSection />
-      <HelpSection />
+      <InvitedUsersSection
+        inviteList={inviteList}
+        setInviteList={setInviteList}
+      />
+      <HelpSection isWidget={isWidget} />
     </div>
   );
 };
