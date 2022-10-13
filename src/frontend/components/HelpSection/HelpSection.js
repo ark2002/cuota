fdd92a5259d9fe6bea61307998c2ea02b05e339a
Aryak Lahane <67817330+ark2002@users.noreply.github.com>
2022-10-13 20:26:05 +0530

    refactor : changes in HelpSection component

--- src/frontend/components/HelpSection/HelpSection.js
+++ src/frontend/components/HelpSection/HelpSection.js
@@ -1,16 +1,18 @@
 import "./HelpSection.css";
 
-const HelpSection = () => {
+const HelpSection = ({ isWidget }) => {
   return (
     <div className="base-widget__bottom flex__row-center">
       <div className="base-widget__help flex__row-center">
         <span className="material-icons help__icon">help_outline</span>
         <p className="widget__text-small">learn about sharing</p>
       </div>
-      <div className="base-widget__copy-link flex__row-center">
-        <span className="material-icons link__icon">link</span>
-        <p className="widget__text-small bold">Copy Link</p>
-      </div>
+      {isWidget === "base" && (
+        <div className="base-widget__copy-link flex__row-center">
+          <span className="material-icons link__icon">link</span>
+          <p className="widget__text-small bold">Copy Link</p>
+        </div>
+      )}
     </div>
   );
 };
