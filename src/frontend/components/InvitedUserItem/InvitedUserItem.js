37381379ee11571a648c391813ef18fe174398c0
Aryak Lahane <67817330+ark2002@users.noreply.github.com>
2022-10-13 20:26:27 +0530

    chore : cleanup in InvitedUserItem

--- src/frontend/components/InvitedUserItem/InvitedUserItem.js
+++ src/frontend/components/InvitedUserItem/InvitedUserItem.js
@@ -1,12 +1,12 @@
 import "./InvitedUserItem.css";
 
 const InvitedUserItem = ({ user }) => {
-  const { id, name, profileType, memberCount, profileImg, access } = user;
+  const { name, profileType, memberCount, profileImg, access } = user;
   return (
     <div className="base-widget__invited-item flex__row-center">
-      <img src={profileImg} alt="oslash__logo" className="invited-user__img" />
+      <img src={profileImg} alt="oslash__logo" className="user__img" />
       <div className="invited-user__details flex__column">
-        <p className="widget__text-large bold">{name}</p>
+        <p className="widget__text-large bold user__name">{name}</p>
         {profileType === "group" && (
           <p className="widget__text-small">{memberCount} workspace members</p>
         )}
