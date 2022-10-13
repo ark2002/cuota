36ea107aac2ea4dcf08f691c05c4ecc262612034
Aryak Lahane <67817330+ark2002@users.noreply.github.com>
2022-10-13 20:28:28 +0530

    feat : created SuggestedUserSection component

---
+++ src/frontend/components/SuggestedUserSection/SuggestedUserSection.js
@@ -0,0 +1,15 @@
+import { SuggestedUserItem } from "../SuggestedUserItem/SuggestedUserItem";
+import "./SuggestedUserSection.css";
+
+const SuggestedUserSection = ({ type, list }) => {
+  return (
+    <div className="search-widget__suggestions flex__column">
+      <p className="widget__text-large suggestions__heading">Select a {type}</p>
+      {list.map((user) => (
+        <SuggestedUserItem user={user} />
+      ))}
+    </div>
+  );
+};
+
+export { SuggestedUserSection };
