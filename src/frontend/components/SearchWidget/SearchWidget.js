32f0a5f9e11a88cf8d337d29425df775d2095022
Aryak Lahane <67817330+ark2002@users.noreply.github.com>
2022-10-13 20:28:46 +0530

    feat : created SearchWidget component

---
+++ src/frontend/components/SearchWidget/SearchWidget.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { useInvite } from "../../context/InviteContext";
+import { HelpSection } from "../HelpSection/HelpSection";
+import { SearchInput } from "../SearchInput/SearchInput";
+import { SuggestedUserSection } from "../SuggestedUserSection/SuggestedUserSection";
+import "./SearchWidget.css";
+
+const SearchWidget = ({ isWidget }) => {
+  const [search, setSearch] = useState("");
+
+  const {
+    list: { userList, groupList },
+  } = useInvite();
+
+  return (
+    <div className="search-widget__container flex__column-center">
+      <SearchInput isWidget={isWidget} search={search} setSearch={setSearch} />
+      {userList.length || groupList.length > 0 ? (
+        <>
+          {userList.length > 0 && (
+            <SuggestedUserSection type={"person"} list={userList} />
+          )}
+          {groupList.length > 0 && (
+            <SuggestedUserSection type={"group"} list={groupList} />
+          )}
+        </>
+      ) : (
+        <h2>no results :( </h2>
+      )}
+      <div className="help__section">
+        <HelpSection isWidget={isWidget} />
+      </div>
+    </div>
+  );
+};
+
+export { SearchWidget };
