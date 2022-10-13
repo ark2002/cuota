76cb672a71bec6afce036c23de66b40158b16959
Aryak Lahane <67817330+ark2002@users.noreply.github.com>
2022-10-13 20:29:10 +0530

    feat : added searchWidget to Main

--- src/frontend/components/Main/Main.js
+++ src/frontend/components/Main/Main.js
@@ -1,16 +1,22 @@
 import { useState } from "react";
 import { BaseWidget } from "../BaseWidget/BaseWidget";
+import { SearchWidget } from "../SearchWidget/SearchWidget";
 import { ShareButton } from "../ShareButton/ShareButton";
 import "./Main.css";
 
 const Main = () => {
-  const [isWidget, setIsWidget] = useState(null);
+  const [isWidget, setIsWidget] = useState("search");
+
   return (
     <div className="main__container">
       <ShareButton isWidget={isWidget} setIsWidget={setIsWidget} />
       {isWidget && (
         <div className="widget__container">
-          <BaseWidget isWidget={isWidget} setIsWidget={setIsWidget} />
+          {isWidget === "base" ? (
+            <BaseWidget isWidget={isWidget} setIsWidget={setIsWidget} />
+          ) : (
+            <SearchWidget isWidget={isWidget} />
+          )}
         </div>
       )}
     </div>
