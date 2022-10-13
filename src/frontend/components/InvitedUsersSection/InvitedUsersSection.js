3590981a67f7e889ac2df615f96ee0c8626d8764
Aryak Lahane <67817330+ark2002@users.noreply.github.com>
2022-10-13 20:27:18 +0530

    refactor : updated InvitedUserSection

--- src/frontend/components/InvitedUsersSection/InvitedUsersSection.js
+++ src/frontend/components/InvitedUsersSection/InvitedUsersSection.js
@@ -1,22 +1,7 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
 import { InvitedUserItem } from "../InvitedUserItem/InvitedUserItem";
 import "./InvitedUsersSection.css";
 
-const InvitedUsersSection = () => {
-  const [dataList, setDataList] = useState([]);
-  const [inviteList, setInviteList] = useState([
-    {
-      id: 5,
-      name: "OSlash",
-      profileType: "group",
-      memberCount: 25,
-      profileImg:
-        "https://global-uploads.webflow.com/60c0cec90f5782a19af55867/61516da7bff77f7a1bdbb25e_32%20x%2032.png",
-      access: "No access",
-    },
-  ]);
-
+const InvitedUsersSection = ({ inviteList }) => {
   return (
     <div className="base-widget__invited-section flex__column-center">
       {inviteList.map((user) => (
