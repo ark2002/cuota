5856a6af601de182c77cfc0e1da21ae3ed655494
Aryak Lahane <67817330+ark2002@users.noreply.github.com>
2022-10-13 20:27:37 +0530

    refactor: updated SearchInput

--- src/frontend/components/SearchInput/SearchInput.js
+++ src/frontend/components/SearchInput/SearchInput.js
@@ -1,10 +1,23 @@
+// import { useState } from "react";
+import { useInvite } from "../../context/InviteContext";
 import "./SearchInput.css";
 
 const SearchInput = ({ isWidget }) => {
-    console.log(isWidget);
+  const {
+    list: { searchInput },
+    dispatchList,
+  } = useInvite();
   return (
     <div className="base-widget__search-container flex__row-center">
-      <input className="search__input" placeholder="People, emails, groups" />
+      <input
+        className="search__input"
+        placeholder="People, emails, groups"
+        autoFocus={isWidget === "search"}
+        value={searchInput}
+        onChange={(e) => {
+          dispatchList({ type: "search", payload: e.target.value });
+        }}
+      />
       <div className="search-container__choices flex__row-center">
         {isWidget === "search" && (
           <div className="invited-user__permission flex__row-center">
