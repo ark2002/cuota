2787c84be22c83869dc18fd80d49aa3d3c9ced9b
Aryak Lahane <67817330+ark2002@users.noreply.github.com>
2022-10-14 20:00:42 +0530

    feat : updated SearchInput

--- src/frontend/components/SearchInput/SearchInput.js
+++ src/frontend/components/SearchInput/SearchInput.js
@@ -1,19 +1,31 @@
+import { useState } from "react";
 import { useInvite } from "../../context/InviteContext";
+import { AccessDropdown } from "../AccessDropdown/AccessDropdown";
 import { SelectedUserChip } from "../SelectedUserChip/SelectedUserChip";
 import "./SearchInput.css";
 
-const SearchInput = ({ isWidget }) => {
-  const {
-    list: { searchInput, selectedList },
-    dispatchList,
-  } = useInvite();
+const SearchInput = ({ isWidget, setIsWidget }) => {
+  const { list, dispatchList } = useInvite();
+
+  const { searchInput, selectedList } = list;
+
+  const [isAccess, setIsAccess] = useState({
+    showList: false,
+    selectedAccess: "No access",
+  });
+
+  const { showList, selectedAccess } = isAccess;
 
   return (
     <div className="base-widget__search-container flex__row">
       <div className="search-container__left flex__row-center">
-        {selectedList.map((user) => (
-          <SelectedUserChip user={user} />
-        ))}
+        {isWidget === "search" && (
+          <>
+            {selectedList.map((user) => (
+              <SelectedUserChip user={user} key={user.id} />
+            ))}
+          </>
+        )}
         <input
           className="search__input"
           placeholder="People, emails, groups"
@@ -26,12 +38,28 @@ const SearchInput = ({ isWidget }) => {
       </div>
       <div className="search-container__choices flex__row-center">
         {isWidget === "search" && (
-          <div className="invited-user__permission flex__row-center">
-            <p className="widget__text-small">No access</p>
+          <div
+            className="invited-user__permission flex__row-center"
+            onClick={() => setIsAccess({ ...isAccess, showList: !showList })}
+          >
+            <p className="widget__text-small">{selectedAccess}</p>
             <span className="material-icons expand__icon">expand_more</span>
           </div>
         )}
-        <button className="invite__btn bold">Invite</button>
+        {showList && (
+          <AccessDropdown setIsAccess={setIsAccess} isWidget={isWidget} />
+        )}
+        <button
+          className="invite__btn bold"
+          onClick={() => {
+            dispatchList({ type: "invited", payload: selectedAccess });
+            setIsAccess({ showList: false, selectedAccess: "No access" });
+            setIsWidget("base");
+          }}
+          disabled={selectedList.length < 1}
+        >
+          Invite
+        </button>
       </div>
     </div>
   );
--- src/frontend/components/SearchWidget/SearchWidget.js
+++ src/frontend/components/SearchWidget/SearchWidget.js
@@ -5,7 +5,7 @@ import { SearchInput } from "../SearchInput/SearchInput";
 import { SuggestedUserSection } from "../SuggestedUserSection/SuggestedUserSection";
 import "./SearchWidget.css";
 
-const SearchWidget = ({ isWidget }) => {
+const SearchWidget = ({ isWidget, setIsWidget }) => {
   const [search, setSearch] = useState("");
 
   const {
@@ -14,7 +14,12 @@ const SearchWidget = ({ isWidget }) => {
 
   return (
     <div className="search-widget__container flex__column-center">
-      <SearchInput isWidget={isWidget} search={search} setSearch={setSearch} />
+      <SearchInput
+        isWidget={isWidget}
+        setIsWidget={setIsWidget}
+        search={search}
+        setSearch={setSearch}
+      />
       {userList.length || groupList.length > 0 ? (
         <>
           {userList.length > 0 && (
