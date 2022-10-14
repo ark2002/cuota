d5c61df7e8ce8fa83c2f792bc8a94a866cd3794b
Aryak Lahane <67817330+ark2002@users.noreply.github.com>
2022-10-14 20:00:21 +0530

    feat : added InvitedUserItem

--- src/frontend/components/InvitedUserItem/InvitedUserItem.js
+++ src/frontend/components/InvitedUserItem/InvitedUserItem.js
@@ -1,7 +1,10 @@
+import { useState } from "react";
+import { AccessDropdown } from "../AccessDropdown/AccessDropdown";
 import "./InvitedUserItem.css";
 
 const InvitedUserItem = ({ user }) => {
   const { name, profileType, memberCount, profileImg, access } = user;
+  const [isAccess, setIsAccess] = useState(false);
   return (
     <div className="base-widget__invited-item flex__row-center">
       <img src={profileImg} alt="oslash__logo" className="user__img" />
@@ -11,9 +14,13 @@ const InvitedUserItem = ({ user }) => {
           <p className="widget__text-small">{memberCount} workspace members</p>
         )}
       </div>
-      <div className="invited-user__permission flex__row-center">
+      <div
+        className="invited-user__permission flex__row-center"
+        onClick={() => setIsAccess(!isAccess)}
+      >
         <p className="widget__text-small">{access}</p>
         <span className="material-icons expand__icon">expand_more</span>
+        {isAccess && <AccessDropdown setIsAccess={setIsAccess} name={name} />}
       </div>
     </div>
   );
